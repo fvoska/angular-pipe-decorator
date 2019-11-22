c9bd6df7849f2baea16a58dbe1ff5eb0aa6ba7b0
Filip Voska <filip.voska@infinum.hr>
2019-11-22 09:37:36 +0100

    Rename to pipe-decorator

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to debounce-decorator!');
+    expect(page.getTitleText()).toEqual('Welcome to pipe-decorator!');
   });
 
   afterEach(async () => {
